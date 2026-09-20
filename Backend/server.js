6071f8be679e19ad505952839caa1f3e1ffd815a
Rabindra kushwaha <rabindrakushwaha0001@gmail.com>
2026-09-20 14:55:28 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -61,4 +61,6 @@ app.get('/api/search', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://0.0.0.0:${PORT}`);
+});
